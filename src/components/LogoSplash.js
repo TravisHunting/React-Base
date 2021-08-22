eac4a23ad04eb1a3edc139c069dcc810210cd3ed
Travis Hunting <travis.hunting@gmail.com>
2021-08-22 18:22:28 +1200

    Removing unused code

--- src/components/LogoSplash.js
+++ src/components/LogoSplash.js
@@ -2,18 +2,15 @@ import logo01 from '../static/images/logo01.png';
 import logo02 from '../static/images/logo02.png';
 import '../static/css/App.css';
 
-const imageWidth = window.innerWidth;
-const imageHeight = Math.round(imageWidth * 9 / 16);
-
 function LogoSplash() {
   return (
     <div className="logo-splash">
 
-        <div className="centerleft" >
+        <div className="logo-left" >
         <img src={logo01} className="spin moderate-size" alt="logo"/>
         </div>
 
-        <div className="centerright" style={{zIndex:"1"}}>
+        <div className="logo-right" style={{zIndex:"1"}}>
         <img src={logo02} className="spin-reverse moderate-size" alt="logo" />
         </div>
 
